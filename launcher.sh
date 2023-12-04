b47ae584bf17c18d6d6b007b75b98966149a8246
Michael Verhulst <michael@terminallabs.com>
2023-12-03 21:44:12 -0600

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,3 +1,5 @@
-apt install -y make
-apt install -y unzip
+wget https://github.com/terminal-labs/standard_nib/archive/refs/heads/main.zip
+cd main.zip
+chmode -R 7777 standard_nib-main
+cd standard_nib-main
 make psf

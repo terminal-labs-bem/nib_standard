9aaf924734b82ad47896ba58a8308a1b94093da0
Michael Verhulst <michael@terminallabs.com>
2024-01-28 04:34:02 -0600

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -5,6 +5,7 @@ apt install make -y
 apt install unzip -y
 wget https://github.com/tlbem/nib_standard/archive/refs/heads/main.zip
 unzip main.zip
-chmod -R 7777 standard_nib-main
-cd standard_nib-main
+chmod -R 7777 nib_standard-main
+chmod -R 7777 nib_standard-main
+cd nib_standard
 make venv.python

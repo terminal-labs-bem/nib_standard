c7dc4d26cb6c4d40310259152a4d20a0fc702a8c
Michael Verhulst <michael@terminallabs.com>
2024-01-28 04:31:25 -0600

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -3,8 +3,8 @@ apt install build-essential -y
 apt install linux-headers-$(uname -r) -y
 apt install make -y
 apt install unzip -y
-wget https://github.com/terminal-labs/standard_nib/archive/refs/heads/main.zip
+wget https://github.com/tlbem/nib_standard/archive/refs/heads/main.zip
 unzip main.zip
 chmod -R 7777 standard_nib-main
 cd standard_nib-main
-make psf
+make venv.python

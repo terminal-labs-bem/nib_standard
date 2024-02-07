cc2b87ffd5d2a21c8e0ad82e495fca77a1bf83ba
Michael Verhulst <michael@terminallabs.com>
2024-02-06 23:13:27 -0600

    Update cli.py

--- src/standardnib/cli.py
+++ src/standardnib/cli.py
@@ -13,24 +13,5 @@ from os.path import isdir, dirname, realpath, abspath, join, exists
 from zipfile import ZipFile
 from configparser import ConfigParser
 
-from lowkit.utils import _delete_dir, _copy_dir
-from lowkit.initialization.workingset import setup_workingset
-from lowkit.functions.helpers import dl_zip, unzip
-
-import os
-import sys
-from os import listdir
-from os.path import isfile, join
-import importlib
-import importlib.util
-import os
-import shutil
-import urllib
-import subprocess
-from urllib.request import urlopen
-from os.path import isdir, dirname, realpath, abspath, join, exists
-from zipfile import ZipFile
-from configparser import ConfigParser
-
 def cli():
 	pass

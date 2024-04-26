1deb80aa7728f7698e3052c2c7f4aaa49958996d
Michael Verhulst <michael@terminallabs.com>
2024-04-25 23:15:27 -0500

    Update builddeb.sh

--- builddeb.sh
+++ builddeb.sh
@@ -1,3 +1,4 @@
+su - vagrant << EOF
 cd /vagrant
 python3 -m venv .builddebvenv
 . .builddebvenv/bin/activate
@@ -5,3 +6,4 @@ pip install toml
 pip install pyclean
 PYTHONUNBUFFERED=true PYTHONPYCACHEPREFIX=/tmp/pycachep2 python3.11 builddeb.py build -p template
 rm -rf .builddebvenv
+EOF
